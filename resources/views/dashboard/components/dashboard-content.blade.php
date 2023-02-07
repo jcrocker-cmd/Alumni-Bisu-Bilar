302baeff1ae67b0d540b8c46c6b999f7628608b6
John Christian B. Narbaja <narbajajc@gmail.com>
2023-02-07 15:43:54 +0800

    Modified Dashboard and Client

--- resources/views/dashboard/components/dashboard-content.blade.php
+++ resources/views/dashboard/components/dashboard-content.blade.php
@@ -2,105 +2,39 @@
         <h2 class="fs-5">Welcome Back <span>John</span> (Administrator)</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore voluptas iste nemo quasi aperiam ducimus sed culpa, in, praesentium iure non aliquam veniam neque! In molestias eveniet laudantium eius facere!</p>
       
-      <div class="row px-3 pt-2 d-flex justify-content-between" id="dashboard-row">
+      <div class="row pt-2 d-flex justify-content-between" id="dashboard-row">
       
+        <div class="dashboard-tab-wrapper">
 
-      <div class="row-card db-card">
         <a href="http://" class="text-decoration-none">
-      <div class="row-card text-white bg-primary mb-3 py-2 px-2">
-        <div class="col-1 card-body">
-            <h5 class="card-title fs-3">10</h5>
-            <p class="card-text fs-6">Costumers</p>
-        </div>
-
-        <div class="col-2 card-body">
-        <i class="fas fa-user fa-3x"></i>
-        </div>
-
-      </div>
-      </a>
-      </div>
-
-      <div class="row-card db-card">
-        <a href="/rented" class="text-decoration-none">
-        <div class="row-card text-white bg-success mb-3 py-2 px-2">
-        <div class="col-1 card-body">
-            <h5 class="card-title fs-3">3</h5>
-            <p class="card-text fs-6">Rented Cars</p>
-        </div>
-
-        <div class="col-2 card-body">
-        <i class="fas fa-car fa-3x"></i>
-        </div>
-
-      </div>
-      </a>
-      </div>
-
-      <div class="row-card db-card">
-        <a href="http://" class="text-decoration-none">
-        <div class="row-card text-white bg-danger mb-3 py-2 px-2">
-        <div class="col-1 card-body">
-            <h5 class="card-title fs-3">20</h5>
-            <p class="card-text fs-6">Available Cars</p>
-        </div>
-
-        <div class="col-2 card-body">
-        <i class="fas fa-car-building fa-3x"></i>
-        </div>
-
-      </div>
-      </a>
-      </div>
-
-      <div class="row-card  db-card">
-        <a href="http://" class="text-decoration-none">
-        <div class="row-card text-white bg-warning mb-3 py-2 px-2">
-        <div class="col-1 card-body">
-            <h5 class="card-title fs-3">99,999</h5>
-            <p class="card-text fs-6">Income</p>
+          <div class="bg-dark text-white dashboard-tab">
+            <p>Alumni Members</p>
+            <p>100</p>
           </div>
+        </a>
 
-        <div class="col-2 card-body">
-        <i class="fas fa-hand-holding-usd fa-3x"></i>
-        </div>
+        <a href="http://" class="text-decoration-none">
+          <div class="bg-dark text-white dashboard-tab">
+            <p>Alumni ID Production</p>
+            <p>100</p>
+          </div>
+        </a>
 
-      </div>
-      </a>
-      </div>
-    
+        <a href="http://" class="text-decoration-none">
+          <div class="bg-dark text-white dashboard-tab">
+            <p>Record for Information of Alumni Students</p>
+            <p>100</p>
+          </div>
+        </a>
 
+        <a href="http://" class="text-decoration-none">
+          <div class="bg-dark text-white dashboard-tab">
+            <p>Announcements</p>
+            <p>100</p>
+          </div>
+        </a>
 
-      <!-- <div class="row-card text-white bg-success mb-3 py-2">
-        <div class="col-1 card-body">
-            <h5 class="card-title fs-3">3</h5>
-            <p class="card-text fs-6">Rented Cars</p>
         </div>
-        <div class="col-2 card-body">
-        <i class="fas fa-car fa-3x"></i>
-        </div>
-      </div>
-
-      <div class="row-card text-white bg-danger mb-3 py-2">
-        <div class="col-1 card-body">
-            <h5 class="card-title fs-3">20</h5>
-            <p class="card-text fs-6">Available Cars</p>
-        </div>
-        <div class="col-2 card-body">
-        <i class="fas fa-car-building fa-3x"></i>
-        </div>
-      </div>
-
-      <div class="row-card text-white bg-warning mb-3 py-2">
-        <div class="col-1 card-body">
-            <h5 class="card-title fs-3">99,999</h5>
-            <p class="card-text fs-6">Income</p>
-        </div>
-        <div class="col-2 card-body">
-        <i class="fas fa-hand-holding-usd fa-3x"></i>
-        </div>
-      </div> -->
-
 
 
       </div>
